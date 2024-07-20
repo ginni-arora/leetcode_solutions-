3980616d79374d463d27798a5b0a61657eeb8363
ginni-arora <148436144+ginni-arora@users.noreply.github.com>
2024-07-20 18:31:10 +0530

    Add files via upload

---
+++ search_insert_position.cpp
@@ -0,0 +1,11 @@
+
+
+int searchInsert(std::vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
+    return nums.size();
+}
+
